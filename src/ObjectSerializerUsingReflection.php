cd5d656def314b1dde5999d0be4b1e2fd6f02eb9
Frank de Jonge <info@frankdejonge.nl>
2022-05-31 13:43:20 +0200

    Serialize guided by a definition.

--- src/ObjectSerializerUsingReflection.php
+++ src/ObjectSerializerUsingReflection.php
@@ -5,102 +5,96 @@ declare(strict_types=1);
 namespace EventSauce\ObjectHydrator;
 
 use BackedEnum;
-use ReflectionAttribute;
-use ReflectionClass;
-use ReflectionMethod;
-use ReflectionProperty;
-use ReflectionUnionType;
 use Throwable;
 use UnitEnum;
 
+use function array_keys;
 use function array_pop;
-use function array_reverse;
 use function count;
 use function get_class;
+use function gettype;
 use function is_a;
 use function is_object;
+use function json_encode;
 
 class ObjectSerializerUsingReflection implements ObjectSerializer
 {
-    private DefaultSerializerRepository $serializers;
-    private KeyFormatter $keyFormatter;
+    /** @var array<string, PropertySerializer */
+    private array $serializerCache = [];
+    private SerializationDefinitionProvider $serializationProvider;
 
     public function __construct(
-        DefaultSerializerRepository $serializers = null,
-        KeyFormatter $keyFormatter = null,
+        SerializationDefinitionProvider $definitionProvider = null,
     ) {
-        $this->serializers = $serializers ?? DefaultSerializerRepository::builtIn();
-        $this->keyFormatter = $keyFormatter ?? new KeyFormatterForSnakeCasing();
+        $this->serializationProvider = $definitionProvider ?? new SerializationDefinitionProvider();
     }
 
     public function serializeObject(object $object): mixed
     {
-        $result = [];
         $className = get_class($object);
 
         try {
-            if ($serializer = $this->serializers->serializerForType($className)) {
+            if ($serializer = $this->serializationProvider->provideSerializer($className)) {
+                /** @var class-string<PropertySerializer> $serializerClass */
                 [$serializerClass, $arguments] = $serializer;
+                $cacheKey = $serializerClass . json_encode($arguments);
+                $this->serializerCache[$cacheKey] ??= new $serializerClass(...$arguments);
 
-                return (new $serializerClass(...$arguments))->serialize($object, $this);
+                return $this->serializerCache[$cacheKey]->serialize($object, $this);
             }
 
-            $reflection = new ReflectionClass($className);
-            $publicMethod = $reflection->getMethods(ReflectionMethod::IS_PUBLIC);
+            $result = [];
+            $definition = $this->serializationProvider->provideDefinition($className);
 
-            foreach ($publicMethod as $method) {
-                if ($method->isStatic() || $method->getNumberOfParameters() !== 0) {
-                    continue;
+            /** @var PropertySerializationDefinition $property */
+            foreach ($definition->properties as $property) {
+                $keys = $property->keys;
+                $accessorName = $property->accessorName;
+
+                $value = $property->type === PropertySerializationDefinition::TYPE_METHOD
+                    ? $object->{$accessorName}()
+                    : $object->{$accessorName};
+
+                if ($value === null || count($property->serializers) === 0) {
+                    goto assign_result;
                 }
 
-                $methodName = $method->getShortName();
-                $returnType = $method->getReturnType();
-                $key = $this->keyFormatter->propertyNameToKey($methodName);
-                $value = $method->invoke($object);
-                $attributes = $method->getAttributes();
-                $value = $this->serializeValue(
-                    $returnType->getName(),
-                    $returnType->isBuiltin(),
-                    $value,
-                    $attributes
-                );
+                $serializers = $property->serializers;
 
-                $this->assignToResult($key, $attributes, $result, $value);
-            }
+                if (array_keys($serializers)[0] === 0) {
+                    foreach ($serializers as $serializer) {
+                        /** @var class-string<PropertySerializer> $serializerClass */
+                        [$serializerClass, $arguments] = $serializer;
+                        $cacheKey = $serializerClass . json_encode($arguments);
+                        $this->serializerCache[$cacheKey] ??= new $serializerClass(...$arguments);
 
-            $publicProperties = $reflection->getProperties(ReflectionProperty::IS_PUBLIC);
-
-            foreach ($publicProperties as $property) {
-                if ($property->isStatic()) {
-                    continue;
-                }
-
-                $key = $this->keyFormatter->propertyNameToKey($property->getName());
-                $value = $property->getValue($object);
-                $propertyType = $property->getType();
-                $attributes = $property->getAttributes();
-
-                if ($propertyType instanceof ReflectionUnionType) {
-                    foreach ($propertyType->getTypes() as $namedType) {
-                        if (is_a($value, $namedType->getName())) {
-                            $value = $this->serializeValue(
-                                $namedType->getName(),
-                                $namedType->isBuiltin(),
-                                $value,
-                                $attributes
-                            );
-                        }
+                        $value = $this->serializerCache[$cacheKey]->serialize($value, $this);
                     }
                 } else {
-                    $value = $this->serializeValue(
-                        $propertyType->getName(),
-                        $propertyType->isBuiltin(),
-                        $value,
-                        $attributes
-                    );
+                    foreach ($serializers as $valueType => $serializer) {
+                        if (is_a($value, $valueType, false) === false && $valueType !== gettype($valueType)) {
+                            continue;
+                        }
+
+                        /** @var class-string<PropertySerializer> $serializerClass */
+                        [$serializerClass, $arguments] = $serializer;
+                        $cacheKey = $serializerClass . json_encode($arguments);
+                        $this->serializerCache[$cacheKey] ??= new $serializerClass(...$arguments);
+                        $value = $this->serializerCache[$cacheKey]->serialize($value, $this);
+                    }
                 }
 
-                $this->assignToResult($key, $attributes, $result, $value);
+                assign_result:
+
+                if ($value instanceof BackedEnum) {
+                    $value = $value->value;
+                } elseif ($value instanceof UnitEnum) {
+                    $value = $value->name;
+                } elseif (is_object($value)) {
+                    $value = $this->serializeObject($value);
+                }
+
+                $this->assignToResult($keys, $result, $value);
             }
 
             return $result;
@@ -109,69 +103,8 @@ class ObjectSerializerUsingReflection implements ObjectSerializer
         }
     }
 
-    /**
-     * @param ReflectionAttribute[] $attributes
-     */
-    private function serializeValue(string $type, bool $builtIn, mixed $value, array $attributes): mixed
+    private function assignToResult(array $keys, array &$result, mixed $value): void
     {
-        $serializers = [];
-
-        foreach ($attributes as $attribute) {
-            $t = $attribute->getName();
-
-            if (is_a($t, PropertySerializer::class, true)) {
-                $serializers[] = [$attribute->getName(), $attribute->getArguments()];
-            }
-        }
-
-        $serializers = array_reverse($serializers, true);
-
-        if (count($serializers) === 0 && $default = $this->serializers->serializerForType($type)) {
-            $serializers[] = $default;
-        }
-
-        if (count($serializers) !== 0) {
-            foreach ($serializers as $serializer) {
-                [$serializerClass, $arguments] = $serializer;
-                /** @var PropertySerializer $serializer */
-                $serializer = new $serializerClass(...$arguments);
-                $value = $serializer->serialize($value, $this);
-            }
-        } elseif ( ! $builtIn) {
-            if ($value instanceof BackedEnum) {
-                return $value->value;
-            } elseif ($value instanceof UnitEnum) {
-                return $value->name;
-            } elseif (is_object($value)) {
-                return $this->serializeObject($value);
-            }
-        }
-
-        return $value;
-    }
-
-    /**
-     * @param ReflectionAttribute[] $attributes
-     *
-     * @return array<string, array<string>>
-     */
-    private function resolveKeys(string $defaultKey, array $attributes): array
-    {
-        foreach ($attributes as $attribute) {
-            if ($attribute->getName() === MapFrom::class) {
-                /** @var MapFrom $mapFrom */
-                $mapFrom = $attribute->newInstance();
-
-                return $mapFrom->keys;
-            }
-        }
-
-        return [$defaultKey => [$defaultKey]];
-    }
-
-    private function assignToResult(string $key, array $attributes, array &$result, mixed $value): void
-    {
-        $keys = $this->resolveKeys($key, $attributes);
         $mapFromSingleKey = count($keys) === 1;
 
         foreach ($keys as $payloadKey => $toPath) {
