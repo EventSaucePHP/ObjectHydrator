aa44d85b46ad550c78f5c282f422c6b7395b71a6
Frank de Jonge <info@frankdejonge.nl>
2022-06-03 21:11:16 +0200

    Remove unneeded import

--- src/PropertyType.php
+++ src/PropertyType.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace EventSauce\ObjectHydrator;
 
 use BackedEnum;
-use EventSauce\ObjectHydrator\FixturesFor81\IntegerEnum;
 use ReflectionClass;
 use ReflectionIntersectionType;
 use ReflectionNamedType;
